406b2ac251b3d64c550dd02e2649b620727fd59c
Victor <>
2023-05-29 22:37:59 -0300

    Start of A* implementation

---
+++ Solver/AStar.cs
@@ -0,0 +1,37 @@
+﻿using Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class AStar
+    {
+        private State InitialState { get; set; }
+        private PriorityQueue<State, int> Open { get; set; } = new PriorityQueue<State, int>();
+
+        public AStar(Board board)
+        {
+            InitialState = new State(board);
+            Open.Enqueue(InitialState, InitialState.GetEvaluationFunction());
+        }
+
+        #region Public Methods
+        public void Solve()
+        {
+            while (Open.Count > 0)
+            {
+                State state = Open.Dequeue();
+
+            }
+        }
+        #endregion
+
+
+        #region Private Methods
+        
+        #endregion
+    }
+}
---
+++ Solver/State.cs
@@ -0,0 +1,33 @@
+﻿using Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class State
+    {
+        public Board Board { get; set; }
+        public State? Parent { get; set; }
+        public int PathCost { get; set; }
+
+        public State(Board board, State? parent = null)
+        {
+            Board = board;
+            Parent = parent;
+            PathCost = Parent != null ? Parent.PathCost + 1 : 0;
+        }
+
+        public int GetHeuristic()
+        {
+            return 0;
+        }
+
+        public int GetEvaluationFunction()
+        {
+            return PathCost + GetHeuristic();
+        }
+    }
+}
